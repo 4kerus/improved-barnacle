1c9356d850b40d1de50877807ece26fd4d0a4919
4kerus@gmail.com <4kerus@gmail.com>
2025-05-01 11:20:47 +0300

    routes fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,18 +2,18 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * Define your route model bindings, pattern filters, and other route configuration.
-     *
-     * @return void
-     */
+
+    public const HOME = '/api';
+
     public function boot(): void
     {
+        parent::boot();
+
         $this->routes(function () {
             // API routes
             Route::middleware('api')
@@ -26,13 +26,4 @@ class RouteServiceProvider extends ServiceProvider
         });
     }
 
-    /**
-     * Configure the rate limiters for the application.
-     *
-     * @return void
-     */
-    protected function configureRateLimiting()
-    {
-
-    }
 }
